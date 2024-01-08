fec7bd9def87178f633058bdb3ae07650ad75f08
Markus Liebelt <mliebelt@hey.com>
2024-01-08 10:30:06 +0100

    #7 Removed mocha and chai and corresponding tests

--- test/chessjs.ts
+++ test/chessjs.ts
@@ -1,18 +1,26 @@
-import {Chess} from 'chess.js'
-import {describe} from "mocha"
-const should = require('chai').should()
+import { suite } from 'uvu';
+import * as assert from 'uvu/assert';
 
-describe("Should handle the API of chess.js", function (){
-    let chess = Chess()
-    it("when evaluating a move (with sloppy)", function () {
-        chess.load("r1bqkbnr/ppp1pppp/2P5/8/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 3")
-        let move = chess.move("bxc6", { sloppy: true})
-        should.equal(move, null)
-        //TODO: This should not be valid. This is a legal notation, and works without sloppy.
-    })
-    it("when evaluating a move (without sloppy)", function () {
-        chess.load("r1bqkbnr/ppp1pppp/2P5/8/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 3")
-        let move = chess.move("bxc6")
-        should.equal(move.san, 'bxc6')
-    })
-})
+import { Chess } from 'chess.js';
+
+const chessjs = suite('chess.js');
+
+chessjs('evaluates move with sloppy', () => {
+    const chess = new Chess();
+    chess.load('r1bqkbnr/ppp1pppp/2P5/8/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 3');
+
+    const move = chess.move('bxc6', { sloppy: true });
+
+    assert.is(move, null);
+});
+
+chessjs('evaluates move without sloppy', () => {
+    const chess = new Chess();
+    chess.load('r1bqkbnr/ppp1pppp/2P5/8/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 3');
+
+    const move = chess.move('bxc6');
+
+    assert.is(move.san, 'bxc6');
+});
+
+chessjs.run();
